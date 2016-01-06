9c0a9452ae02c83544ee95fc7d3cb624148b73dd
Romain CHARTIER <chartier.romain@gmail.com>
2016-01-06 17:39:01 +0100

    Push

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@
 docker login
 docker build -t $(basename $(pwd)) .
 docker tag $(basename $(pwd)):latest $(basename $(pwd)):$(git describe --always --tags)
+docker push $(basename $(pwd))
+docker push $(basename $(pwd)):$(git describe --always --tags)
+
